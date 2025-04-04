059599a1c37cbda99c38dd0902463a8c20f86a96
Mathiyas Anthony <mathiyas.anthony@ibm.com>
2025-04-04 16:35:21 +0530

    Remove routes

--- shell-poc/src/App.js
+++ shell-poc/src/App.js
@@ -5,12 +5,7 @@ import Dashboard from './Dashboard'; // Adjust the import path as necessary
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* Add other routes here */}
-      </Routes>
-    </Router>
+    <Dashboard />
   );
 }
 
